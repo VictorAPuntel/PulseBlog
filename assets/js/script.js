032261f77f7b58a36247f83cd8f52bfb034b2b2b
VictorAPuntel <victor.a.puntel@gmail.com>
2026-01-07 00:42:47 -0300

    feat: add real-time likes with persistence to posts

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ async function getPosts() {
   try {
     let url = 'https://jsonplaceholder.typicode.com/posts'
     const res = await fetch(url)
+    if (!res.ok) {
+      throw new Error('Something went wrong')
+    }
     const posts = await res.json()
 
     let postSection = document.querySelector('.posts')
@@ -14,11 +17,18 @@ async function getPosts() {
       p.textContent = post.body
 
       const button = document.createElement('button')
+      button.addEventListener('click', () => {
+        addLike(post.id)
+        const likeCount = getLikesFromId(post.id)
+        let word = likeCount === 1 ? 'Like' : 'Likes'
+        small.textContent = `${likeCount} ${word}`
+      })
       button.textContent = 'Like'
 
       const small = document.createElement('small')
       const likeCount = getLikesFromId(post.id)
-      small.textContent = `${likeCount} Likes`
+      let word = likeCount === 1 ? 'Like' : 'Likes'
+      small.textContent = `${likeCount} ${word}`
 
       let article = document.createElement('article')
       article.append(h2, p, button, small)
@@ -39,5 +49,22 @@ function getLikesFromId(id) {
   return postLike.count
 }
 
+function addLike(id) {
+  let likesString = localStorage.getItem('likes')
+  if (!likesString) {
+    likesString = '[]'
+  }
+
+  const likes = JSON.parse(likesString)
+  const index = likes.findIndex((item) => item.id === id)
+
+  if (index > -1) {
+    likes[index].count++
+  } else {
+    likes.push({ id: id, count: 1 })
+  }
+
+  localStorage.setItem('likes', JSON.stringify(likes))
+}
 
 getPosts()
